e7bc85637674eb8f2cead9d84dd8d53ebd32ed63
realmags <kentbryanmaglinao9@gmail.com>
2022-06-22 14:51:38 +0800

    chore: Add 'featured' section in home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,15 @@
 import { StaticImage } from "gatsby-plugin-image"
 import PageContainer from "../components/PageContainer"
-import { FaFacebookF, FaDiscord, FaTwitter, FaRegStar } from "react-icons/fa"
+import {
+  FaFacebookF,
+  FaDiscord,
+  FaTwitter,
+  FaRegStar,
+  FaRegNewspaper,
+} from "react-icons/fa"
 import { BsArrowRight } from "react-icons/bs"
 import { Link } from "gatsby"
+import { Fragment } from "react"
 
 const platforms = [
   {
@@ -19,6 +26,22 @@ const platforms = [
     icon: FaTwitter,
     text: "Twitter",
     url: "",
+  },
+]
+
+const posts = [
+  {
+    title: "Good news! CpE certification finally underway for new grads",
+    lead: "Computer Engineering professionals and graduates alike can now apply for certification.",
+    link: "#",
+    thumbnail: false,
+  },
+  {
+    title: "Lore unfolds: CpE Days '22 finally underway",
+    lead: "After an overwhelming academic year, we deserve some unwinding. It is our time to gather as a pact and venture the night of Silverwood.",
+    link: "#",
+    // todo: thumbnail is not boolean
+    thumbnail: true,
   },
 ]
 
@@ -72,7 +95,46 @@ function IndexPage() {
         </div>
       </section>
 
-      <section className="border-t-2 border-black">FEATURED</section>
+      {/* FEATURED */}
+      <section className="border-t-2 border-black">
+        <div className="flex flex-col bg-white px-10 py-8">
+          <span className="flex items-center gap-x-3">
+            <FaRegNewspaper className="md:h-6 md:w-6" />
+            <h2 className="text-lg font-light lg:text-2xl">Featured</h2>
+          </span>
+          <div className="my-8 grid grid-cols-1 place-items-center gap-y-12 gap-x-0 px-6 md:my-0 md:grid-cols-[1fr_6rem_1fr]">
+            {posts.map(({ title, lead, link, thumbnail }, index) => {
+              return (
+                <Fragment key={title}>
+                  {index % 2 !== 0 && (
+                    <hr className="border-px w-4/6 border-black md:w-[200%] md:rotate-90" />
+                  )}
+                  <div className="relative flex h-fit flex-col">
+                    {thumbnail && (
+                      <StaticImage
+                        src="../images/home/cpe-days-2022.jpg"
+                        className="md:h-36 lg:h-52"
+                        alt={title}
+                      />
+                    )}
+                    <h4 className="my-4 font-libre text-base font-bold leading-tight">
+                      <Link to={link}>{title}</Link>
+                    </h4>
+                    <p className="font-montserrat leading-tight">{lead}</p>
+                    <Link
+                      to={link}
+                      className="mt-6 flex items-center gap-x-3 self-end font-montserrat font-semibold"
+                    >
+                      <small>Read More</small>
+                      <BsArrowRight />
+                    </Link>
+                  </div>
+                </Fragment>
+              )
+            })}
+          </div>
+        </div>
+      </section>
 
       <section className="border-t-2 border-black px-4 py-10 font-montserrat text-sm md:px-8 md:text-base xl:px-12 xl:py-20 xl:text-2xl">
         <header className="mb-4 xl:mb-8">
