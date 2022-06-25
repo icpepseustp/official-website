cb363b28565af63055cb280a5ae674ccc6943152
Marvin Tagolimot Jr <31147592+diffident016@users.noreply.github.com>
2022-06-25 12:50:51 +0800

    feat(pages): Initial about page (#7)
    Co-authored-by: Danry Ague <74456102+decanTyme@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 const routes = [
   { text: "Home", path: "/" },
   { text: "Blog", path: "/#blog" },
-  { text: "About", path: "/#about" },
+  { text: "About", path: "/about" },
   { text: "Contact", path: "/#contact" },
 ]
 
