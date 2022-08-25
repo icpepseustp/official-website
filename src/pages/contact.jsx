2245510ce96fc33912ec823b2803716818d18577
Kurt Timajo <djkurttimajo@gmail.com>
2022-08-26 00:10:18 +0800

    feat(page): revised contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,10 +2,12 @@
 /* eslint-disable react/prop-types */
 import { graphql } from "gatsby"
 import { FaDiscord, FaFacebookF, FaTwitter } from "react-icons/fa"
-import { SiGmail, SiGooglemessages } from "react-icons/si"
+import { SiGmail } from "react-icons/si"
 import { ImLocation } from "react-icons/im"
 import { AiFillInstagram } from "react-icons/ai"
+import { MdMessage } from "react-icons/md"
 import Seo from "../components/Seo"
+import { StaticImage } from "gatsby-plugin-image"
 
 // dawdad
 function ContactPage({ data }) {
@@ -15,17 +17,17 @@ function ContactPage({ data }) {
       <Seo title="Contact" />
 
       <div className="px-2 py-6 lg:px-12">
-        <div className="font-PS2P">
-          <h1 className="shrink text-center text-base font-medium md:text-xl lg:text-left lg:text-4xl">
+        <div className="text-center font-PS2P">
+          <h1 className="shrink text-base font-medium md:text-xl  lg:text-4xl">
             GET IN TOUCH WITH US!
           </h1>
-          <p className="my-4 shrink text-center text-xs md:text-sm lg:text-left lg:text-base">
+          <p className="my-4 shrink text-center text-xs md:text-sm  lg:text-base">
             REACH OUT TO OUR DEPARTMENT
           </p>
         </div>
         <div className="flex flex-col items-center justify-center">
-          <div className="flex min-w-max flex-col py-8">
-            <h3 className="font-montserrat text-base font-bold md:text-lg lg:text-xl">
+          <div className="z-40 flex min-w-max flex-col py-8 ">
+            <h3 className="bg-gray-900 text-center font-montserrat text-base font-bold text-amber-50 md:text-lg lg:text-xl">
               Send us a message
             </h3>
             <div className="grid shrink grid-cols-1 gap-x-60 gap-y-8 px-8 py-6 lg:grid-cols-2">
@@ -40,16 +42,16 @@ function ContactPage({ data }) {
                 href={`tel:${contact[1].link_address}`}
                 className="flex items-center gap-x-1.5 xl:gap-x-4"
               >
-                <SiGooglemessages className="h-[18px] w-[18px] lg:h-[28px] lg:w-[28px]" />
+                <MdMessage className="h-[18px] w-[18px] lg:h-[28px] lg:w-[28px]" />
                 <small className="text-[16px] lg:text-[18px]">
                   {contact[1].name}
                 </small>
               </a>
             </div>
-            <h3 className="mt-6 font-montserrat text-base font-bold md:text-lg lg:text-xl">
+            <h3 className="mt-6 bg-gray-900 text-center font-montserrat text-base font-bold text-amber-50 md:text-lg lg:bg-inherit  lg:text-xl lg:text-gray-900">
               Share your thoughts with us
             </h3>
-            <div className="grid shrink grid-cols-1 gap-x-60 gap-y-10 py-6 px-8 lg:grid-cols-2">
+            <div className="grid shrink grid-cols-1 gap-x-60 gap-y-10 px-12 py-6 lg:grid-cols-2">
               <a
                 href={contact[2].link_address}
                 className="flex items-center gap-x-1.5 xl:gap-x-4"
@@ -80,6 +82,14 @@ function ContactPage({ data }) {
                 <small className="text-[18px]">{contact[5].name}</small>
               </a>
             </div>
+          </div>
+          <div className="s absolute z-30 -mt-24 opacity-25 lg:-mt-40">
+            <StaticImage
+              width={500}
+              height={500}
+              alt="icpep-no-name"
+              src="../images/icpep-logo-noname.png"
+            />
           </div>
         </div>
       </div>
