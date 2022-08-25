231b1e5833f21651db45572778ebf540aca934e2
diffident016 <marvin.tagolimotjr13@gmail.com>
2022-08-25 21:10:26 +0800

    fix(page): fix screen response for (md) screens

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -11,17 +11,20 @@ function NotFoundPage() {
         <h1 className="mt-12  shrink cursor-default font-PS2P text-4xl font-normal drop-shadow-2xl md:text-6xl lg:text-7xl">
           WHOOPSY!
         </h1>
-        <div className="-mt-1 flex max-w-full shrink flex-col items-center justify-items-center lg:-mt-20 lg:flex-row">
+        <div className="relative -mt-1 flex max-w-full shrink flex-col items-center justify-items-center lg:-mt-20 lg:flex-row">
           <div>
             <div className="flex w-[500px] flex-col items-center px-12 lg:items-start">
-              <p className="font-monsterrat mt-24 mb-2 cursor-default text-[16px] font-semibold md:text-[20px] lg:text-[24px]">
+              <p className="font-monsterrat mt-32 mb-2 cursor-default text-[16px] font-semibold md:text-[20px] lg:mt-24 lg:text-[24px]">
                 Joe couldn&apos;t find that page.
               </p>
               <h2 className="font-monsterrat cursor-default text-2xl font-extrabold md:text-3xl lg:text-5xl">
                 Page Not Found
               </h2>
 
-              <Link to="/" className="mt-24 flex flex-row items-center gap-x-2">
+              <Link
+                to="/"
+                className="mt-28 flex flex-row items-center gap-x-2 lg:mt-24"
+              >
                 <BsArrowLeft className="h-6 w-6 cursor-pointer" />
                 <p className="font-monsterrat cursor-pointer text-sm font-bold hover:text-slate-600 hover:underline active:text-slate-400 active:no-underline md:text-base lg:text-lg">
                   Go Back to Home
